6145da753899ef10fb75a8227f535ff520d6e18a
jianjunhuang <jianjun.huang@mobisummer.com>
2019-03-18 19:51:45 +0800

    add choice layout

---
+++ component_bill/src/main/java/com/jianjunhuang/bill/BillFragment.java
@@ -0,0 +1,33 @@
+package com.jianjunhuang.bill;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.jianjunhuang.bill.databinding.BillFragmentBinding;
+import com.jianjunhuang.common_base.base.BaseFragment;
+
+@Route(path = "/bill/fragment")
+public class BillFragment extends BaseFragment {
+
+  private BillFragmentBinding mBinding;
+
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    mBinding = DataBindingUtil.inflate(inflater, R.layout.bill_fragment, container, false);
+    return mBinding.getRoot();
+  }
+
+  @Override
+  public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+  }
+}
